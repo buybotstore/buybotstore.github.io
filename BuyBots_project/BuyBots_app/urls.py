fea6a46073e8a219cc3434e6ca1f81f02fa9d828
Andrey Naidenko <naidenkoandrey1997@mail.ru>
2018-08-13 16:52:39 +0300

    Изменения в моделях, наводим порядок

--- BuyBots_project/BuyBots_app/urls.py
+++ BuyBots_project/BuyBots_app/urls.py
@@ -8,6 +8,5 @@ urlpatterns = [
     url(r'^bot/(?P<pk>\d+)/$', views.bot_detail, name='bot_detail'),
     url(r'lk/', views.lk, name='lk'),
     url(r'^accounts/', include('django.contrib.auth.urls')),
-    url(r'^bot/(?P<pk>\d+)/$', views.bot_detail, name='bot_detail'),
     url(r'^bot/add/$', views.add_bot, name='add_bot')
 ]
